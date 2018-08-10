d283b43b453b43cf9da689f52e4d1b35911f2203
lusensama <lusensama@gmail.com>
2018-08-09 20:18:11 -0400

    retrain image keypoints

--- un_trim_process.py
+++ un_trim_process.py
@@ -13,77 +13,63 @@ parser.add_argument("-extract_audio_kp", "--extract_audio_kp", type=bool, defaul
 
 if __name__ == '__main__':
 
-    inputVideoFolder = 'videos_cut/'
+    inputVideoFolder = 'input_videos/'
     audioFolder = 'extracted_audios/'
     imageFolder = 'extracted_images/'
     imgkpFolder = 'image_kp_raw/'
     audkpFolder = 'audio_kp/'
     pcaFolder = 'pca/'
-    if not(os.path.exists(audioFolder)):
-        # Create directory
-        subprocess.call('mkdir -p ' + audioFolder, shell=True)
-    if not(os.path.exists(imageFolder)):
-        # Create directory
-        subprocess.call('mkdir -p ' + imageFolder, shell=True)
-    if not(os.path.exists(imgkpFolder)):
-        # Create directory
-        subprocess.call('mkdir -p ' + imgkpFolder, shell=True)
-    if not(os.path.exists(audkpFolder)):
-        # Create directory
-        subprocess.call('mkdir -p ' + audkpFolder, shell=True)
-    if not(os.path.exists(pcaFolder)):
-        # Create directory
-        subprocess.call('mkdir -p ' + pcaFolder, shell=True)
-# if (args.extract_images):
 
-    inputFolder = inputVideoFolder
-    outputFolder = imageFolder
+# # if (args.extract_images):
 
-    if not(os.path.exists(outputFolder)):
-        # Create directory
-        subprocess.call('mkdir -p ' + outputFolder, shell=True)
+#     inputFolder = inputVideoFolder
+#     outputFolder = imageFolder
 
-    filelist = sorted(glob(inputFolder+'/*.mp4'))
+#     if not(os.path.exists(outputFolder)):
+#         # Create directory
+#         subprocess.call('mkdir -p ' + outputFolder, shell=True)
 
-    print('Length of filelist: ', len(filelist))
+#     filelist = sorted(glob(inputFolder+'/*.mp4'))
 
-    for idx, filename in tqdm(enumerate(filelist)):
-        num = filename[len(inputFolder):-len('.mp4')]
-        print('Num: ', num)
-        # Create this directory if it doesn't exist
-        if not(os.path.exists(outputFolder+num)):
-            # Create directory
-            subprocess.call('mkdir -p ' + outputFolder+num, shell=True)
+#     print('Length of filelist: ', len(filelist))
 
-        # Create the images
-        cmd = 'ffmpeg -i ' + filename + ' -vf scale=-1:256 '+ outputFolder + num + '-%05d' + '.bmp'
-        subprocess.call(cmd, shell=True)
+#     for idx, filename in tqdm(enumerate(filelist)):
+#         num = filename[len(inputFolder):-len('.mp4')]
+#         print('Num: ', num)
+#         # Create this directory if it doesn't exist
+#         if not(os.path.exists(outputFolder+num)):
+#             # Create directory
+#             subprocess.call('mkdir -p ' + outputFolder+num, shell=True)
 
-        # Cropping
-        imglist = sorted(glob( outputFolder + num + '/*.bmp'))
+#         # Create the images
+#         cmd = 'ffmpeg -i ' + filename + ' -vf scale=-1:256 '+ outputFolder + num + '-%05d' + '.bmp'
+#         subprocess.call(cmd, shell=True)
 
-        for i in range(len(imglist)):
-            img = cv2.imread(imglist[i])
-            x = int(np.floor((img.shape[1]-256)/2))
-            crop_img = img[0:256, x:x+256]
-            cv2.imwrite( imglist[i][0:-len('.bmp')] + '.jpg', crop_img)
+#         # Cropping
+#         imglist = sorted(glob( outputFolder + num + '/*.bmp'))
 
-        subprocess.call('rm -rf '+ outputFolder + num + '/*.bmp', shell=True)
+#         for i in range(len(imglist)):
+#             img = cv2.imread(imglist[i])
+#             x = int(np.floor((img.shape[1]-256)/2))
+#             crop_img = img[0:256, x:x+256]
+#             cv2.imwrite( imglist[i][0:-len('.bmp')] + '.jpg', crop_img)
 
-# if (args.extract_audio):
+#         subprocess.call('rm -rf '+ outputFolder + num + '/*.bmp', shell=True)
 
-    inputFolder = inputVideoFolder
-    outputFolder = audioFolder
+# # if (args.extract_audio):
 
-    if not(os.path.exists(outputFolder)):
-        # Create directory
-        subprocess.call('mkdir -p ' + outputFolder, shell=True)
+#     inputFolder = inputVideoFolder
+#     outputFolder = audioFolder
 
-    filelist = sorted(glob(inputFolder+'/*.mp4'))
+#     if not(os.path.exists(outputFolder)):
+#         # Create directory
+#         subprocess.call('mkdir -p ' + outputFolder, shell=True)
 
-    for file in filelist:
-        cmd = 'ffmpeg -i ' + file + ' -ab 160k -ac 1 -ar 16000 -vn ' + outputFolder + file[len(inputFolder): -len('.mp4')] + '.wav'
-        subprocess.call(cmd, shell=True)
+#     filelist = sorted(glob(inputFolder+'/*.mp4'))
+
+#     for file in filelist:
+#         cmd = 'ffmpeg -i ' + file + ' -ab 160k -ac 1 -ar 16000 -vn ' + outputFolder + file[len(inputFolder): -len('.mp4')] + '.wav'
+#         subprocess.call(cmd, shell=True)
 
 # if (args.extract_image_kp):
 
@@ -96,7 +82,7 @@ if __name__ == '__main__':
         subprocess.call('mkdir -p ' + outputFolder, shell=True)
 
     # directories = sorted(glob(inputFolder+'*/'))
-    directories = sorted(glob(inputFolder))
+    directories = sorted(glob(inputFolder+"**/"))
     print(directories)
     d = {}
     print("Obtaining facial keypoints")
@@ -105,7 +91,7 @@ if __name__ == '__main__':
         imglist = sorted(glob(directory+'*.bmp'))
         big_list = []
         for file in tqdm(imglist):
-            
+
             keypoints = get_facial_landmarks(file)
             if not (keypoints.shape[0] == 1): # if there are some kp then
                 l = getKeypointFeatures(keypoints)
@@ -136,68 +122,68 @@ if __name__ == '__main__':
             cmd = 'rm -rf ' + oldSaveFilename
             subprocess.call(cmd, shell=True)
 
-# if (args.extract_audio_kp):
+# # if (args.extract_audio_kp):
 
-    inputFolder = audioFolder
-    outputFolder = audkpFolder
-    resumeFrom = 0
-    frame_rate = 5
-    kp_type = 'mel'
+#     inputFolder = audioFolder
+#     outputFolder = audkpFolder
+#     resumeFrom = 0
+#     frame_rate = 5
+#     kp_type = 'mel'
 
-    if not(os.path.exists(outputFolder)):
-        # Create directory
-        subprocess.call('mkdir -p ' + outputFolder, shell=True)
+#     if not(os.path.exists(outputFolder)):
+#         # Create directory
+#         subprocess.call('mkdir -p ' + outputFolder, shell=True)
 
-    filelist = sorted(glob(inputFolder+'*.wav'))
+#     filelist = sorted(glob(inputFolder+'*.wav'))
 
-    d = {}
+#     d = {}
 
-    for idx, file in enumerate(tqdm(filelist[resumeFrom:])):
-        key = file[len(inputFolder):-len('.wav')]
+#     for idx, file in enumerate(tqdm(filelist[resumeFrom:])):
+#         key = file[len(inputFolder):-len('.wav')]
 
-        if(kp_type == 'world'):
-            x, fs = sf.read(file)
-            # 2-1 Without F0 refinement
-            f0, t = pw.dio(x, fs, f0_floor=50.0, f0_ceil=600.0,
-                            channels_in_octave=2,
-                            frame_period=frame_rate,
-                            speed=1.0)
-            sp = pw.cheaptrick(x, f0, t, fs)
-            ap = pw.d4c(x, f0, t, fs)
-            features = np.hstack((f0.reshape((-1, 1)), np.hstack((sp, ap))))
+#         if(kp_type == 'world'):
+#             x, fs = sf.read(file)
+#             # 2-1 Without F0 refinement
+#             f0, t = pw.dio(x, fs, f0_floor=50.0, f0_ceil=600.0,
+#                             channels_in_octave=2,
+#                             frame_period=frame_rate,
+#                             speed=1.0)
+#             sp = pw.cheaptrick(x, f0, t, fs)
+#             ap = pw.d4c(x, f0, t, fs)
+#             features = np.hstack((f0.reshape((-1, 1)), np.hstack((sp, ap))))
 
-        elif (kp_type == 'mel'):
-            (rate, sig) = wav.read(file)
-            features = logfbank(sig,rate)
+#         elif (kp_type == 'mel'):
+#             (rate, sig) = wav.read(file)
+#             features = logfbank(sig,rate)
 
-        d[key] = features
+#         d[key] = features
 
-        saveFilename = outputFolder + 'audio_kp' + str(idx+resumeFrom+1) + '_' + kp_type + '.pickle'
-        oldSaveFilename = outputFolder + 'audio_kp' + str(idx+resumeFrom-2) + '_' + kp_type + '.pickle'
+#         saveFilename = outputFolder + 'audio_kp' + str(idx+resumeFrom+1) + '_' + kp_type + '.pickle'
+#         oldSaveFilename = outputFolder + 'audio_kp' + str(idx+resumeFrom-2) + '_' + kp_type + '.pickle'
 
-        if not (os.path.exists(saveFilename)):
-            with open(saveFilename, "wb") as output_file:
-                pkl.dump(d, output_file)
-                # print('Saved output for', (idx+resumeFrom+1), 'file.')
-        else:
-            # Resume
-            with open(saveFilename, "rb") as output_file:
-                d = pkl.load(output_file)
-                print('Loaded output for ', (idx+resumeFrom+1), ' file.')
+#         if not (os.path.exists(saveFilename)):
+#             with open(saveFilename, "wb") as output_file:
+#                 pkl.dump(d, output_file)
+#                 # print('Saved output for', (idx+resumeFrom+1), 'file.')
+#         else:
+#             # Resume
+#             with open(saveFilename, "rb") as output_file:
+#                 d = pkl.load(output_file)
+#                 print('Loaded output for ', (idx+resumeFrom+1), ' file.')
 
-        # Keep removing stale versions of the files
-        if (os.path.exists(oldSaveFilename)):
-            cmd = 'rm -rf ' + oldSaveFilename
-            subprocess.call(cmd, shell=True)
+#         # Keep removing stale versions of the files
+#         if (os.path.exists(oldSaveFilename)):
+#             cmd = 'rm -rf ' + oldSaveFilename
+#             subprocess.call(cmd, shell=True)
 
-    print('Saved Everything')
+#     print('Saved Everything')
 
 # if (args.extract_pca):
 
     inputFolder = imgkpFolder
     outputFolder = pcaFolder
     # numOfFiles = 1467 # First 20 videos
-    numOfFiles = 1
+    numOfFiles = 98
     new_list = []
 
     filename = inputFolder + 'kp' + str(numOfFiles) + '.pickle'
@@ -245,6 +231,7 @@ if __name__ == '__main__':
         # print('Key:', key)
         nFrames = len(big_list[key])
         factor = int(np.ceil(100/29.97))
+        gg = big_list[key][0][0].shape
         # Create the matrix
         new_unit_kp = np.zeros((int(factor*nFrames), big_list[key][0][0].shape[0], big_list[key][0][0].shape[1]))
         new_kp = np.zeros((int(factor*nFrames), big_list[key][0][-1].shape[0], big_list[key][0][-1].shape[1]))
